266de9e0ca6efaecfffd78b3af09f6ab63fe6f52
Kilian Wilkes <kiwi0kiw@gmail.com>
2021-11-22 19:07:33 +0100

    Extended the Pi-Arduino Dataexchange. Now the user can choose, which data the arduino should send

--- ArduinoToPiDataTransfer/PiDataReceiver.py
+++ ArduinoToPiDataTransfer/PiDataReceiver.py
@@ -4,21 +4,48 @@ class GraphVisualizer:
     '''
     port must be a string like 'COM3'. Retrieve possible ports with GraphVisualizer.list_possible_ports
     '''
-    def __init__(self, port, baudrate=115200, timeout=.1) -> None:
+    def __init__(self, port, baudrate=115200, timeout=.1, send_raw_data = False, send_filtered_data = False, send_envlope = True, data_separation=",") -> None:
         self.arduino = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+        self.send_raw_data = send_raw_data
+        self.send_filtered_data = send_filtered_data
+        self.send_envlope = send_envlope
+        self.data_separation = data_separation
 
-    def write_read(self, x):
+    def init_arduino(self):
+        d = self.data_separation
+
+        if(self.send_raw_data):
+            d += "1"
+        else:
+            d += "0"
+
+        if(self.send_filtered_data):
+            d += "1"
+        else:
+            d += "0"
+        
+        if(self.send_envlope):
+            d += "1"
+        else:
+            d += "0"
+
+        self.write(d)
+
+    def write(self, x):
         self.arduino.write(bytes(x, 'utf-8'))
         return
 
+    '''
+    returns list. List may contain raw_data, filtered_data, envlope, depending on send_raw_data and the other attributes
+    '''
     def read(self):
         data = self.arduino.readline()
         return data
 
     '''
-        staticmethod
-        returns list with items like this: 
-        ('COM4', 'Arduino Due Programming Port (COM4)', 'USB VID:PID=2341:003D SNR=75330303035351300230')
+    staticmethod
+    returns list with items like this: 
+    ('COM4', 'Arduino Due Programming Port (COM4)', 'USB VID:PID=2341:003D SNR=75330303035351300230')
     '''
     @staticmethod
     def list_possible_ports():
